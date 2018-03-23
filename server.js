872cd647e60b52bb2300f8780fcc7977a01faa7a
Peter Han <phan@salesforce.com>
2018-03-23 01:47:20 -0700

    Link up users and streams

--- server.js
+++ server.js
@@ -2,7 +2,26 @@
     'use strict';
 
     var promise = require('bluebird');
+    var _ = require('lodash');
     var twitch = require('./twitch.js');
 
-    twitch.getStreams();
+    var streamerDict = JSON.parse(process.env.STREAMERS) || {};
+    var streamerIds = {};
+
+    twitch.getUsers(Object.keys(streamerDict))
+        .then(function (users) {
+            return promise.try(function () {
+                _.forEach(users, function (user) {
+                    streamerIds[user.id] = user.login;
+                });
+            });
+        })
+        .then(function () {
+            return twitch.getStreams(Object.keys(streamerIds));
+        })
+        .then(function (streams) {
+            _.forEach(streams, function (stream) {
+                console.log([streamerIds[stream.user_id], 'is currently streaming:', stream.title].join(' '));
+            });
+        });
 })();
