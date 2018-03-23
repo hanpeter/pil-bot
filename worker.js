7b90cb8c59ab81dbd28fb6c7c9e338f2fd24f646
Peter Han <phan@salesforce.com>
2018-03-23 02:11:51 -0700

    Adding clock.js

--- worker.js
+++ worker.js
@@ -1,27 +1,36 @@
 (function () {
     'use strict';
 
-    var promise = require('bluebird');
+    var Promise = require('bluebird');
     var _ = require('lodash');
     var twitch = require('./twitch.js');
 
-    var streamerDict = JSON.parse(process.env.STREAMERS) || {};
-    var streamerIds = {};
+    function worker() {
+        var streamerDict = JSON.parse(process.env.STREAMERS) || {};
 
-    twitch.getUsers(Object.keys(streamerDict))
-        .then(function (users) {
-            return promise.try(function () {
-                _.forEach(users, function (user) {
-                    streamerIds[user.id] = user.login;
-                });
-            });
-        })
-        .then(function () {
-            return twitch.getStreams(Object.keys(streamerIds));
-        })
-        .then(function (streams) {
-            _.forEach(streams, function (stream) {
-                console.log([streamerIds[stream.user_id], 'is currently streaming:', stream.title].join(' '));
-            });
-        });
+        return {
+            work: function () {
+                var streamerIds = {};
+
+                twitch.getUsers(Object.keys(streamerDict))
+                    .then(function (users) {
+                        return Promise.try(function () {
+                            _.forEach(users, function (user) {
+                                streamerIds[user.id] = user.login;
+                            });
+                        });
+                    })
+                    .then(function () {
+                        return twitch.getStreams(Object.keys(streamerIds));
+                    })
+                    .then(function (streams) {
+                        _.forEach(streams, function (stream) {
+                            console.log([streamerIds[stream.user_id], 'is currently streaming:', stream.title].join(' '));
+                        });
+                    });
+            }
+        };
+    }
+
+    module.exports = worker();
 })();
