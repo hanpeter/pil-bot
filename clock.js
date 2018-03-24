55c63797e02b6c680f60e6a0ba39681a16cea3b5
Peter Han <phan@salesforce.com>
2018-03-24 11:25:57 -0700

    Initial implementation of rollbar

--- clock.js
+++ clock.js
@@ -3,12 +3,18 @@
 
     var CronJob = require('cron').CronJob;
     var worker = require('./worker.js');
+    var rollbar = require('./rollbar.js');
 
-    var job = new CronJob({
-        cronTime: '* * * * *',  // Run every minute
-        // TODO: Update this to queue the job rather than running it itself
-        onTick: worker.work,
-        timeZone: 'Etc/UTC'
-    });
-    job.start();
+    try {
+        var job = new CronJob({
+            cronTime: '* * * * *',  // Run every minute
+            // TODO: Update this to queue the job rather than running it itself
+            onTick: worker.work,
+            timeZone: 'Etc/UTC'
+        });
+        job.start();
+    } catch (ex) {
+        rollbar.error(ex);
+        throw ex;
+    }
 })();
