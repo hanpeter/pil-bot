02a549b29c025a55fa93703e8bf7730031b8d6e4
Peter Han <phan@salesforce.com>
2018-03-24 15:46:43 -0700

    Comment on error logging

--- worker.js
+++ worker.js
@@ -34,7 +34,11 @@
                         });
                     })
                     .catch(function (error) {
-                        console.log(error);
+                        // GOTCHA: This means errors from API calls are potentially logged twice.
+                        //         However, I do want to catch any exceptions thrown elsewhere without copying & pasting
+                        //         error logging code everywhere. So, this is the most elegant I can come up with
+                        //         at this time. Looking at the Rollbar's source code and based on a small
+                        //         empirical test, looks like they have a logic to prevent duplicate logging.
                         rollbar.error(error);
                         throw error;
                     });
