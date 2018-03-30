911b959bdc23579db1af0b54d7665a495f6cc472
Peter Han <git@peterhan.me>
2018-03-29 18:27:10 -0700

    Rollbar should be optional

--- src/rollbar.js
+++ src/rollbar.js
@@ -2,15 +2,28 @@
     'use strict';
 
     var Rollbar = require('rollbar');
+    var logger = require('./logger.js');
 
     function rollbar() {
-        var rollbar = new Rollbar({
-            accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-            environment: process.env.ENVIRONMENT,
-        });
+        var rollbar = undefined;
+
+        if (process.env.ROLLBAR_ACCESS_TOKEN){
+            rollbar = new Rollbar({
+                accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+                environment: process.env.ENVIRONMENT || 'development',
+            })
+        }
+        else {
+            logger.warn('ROLLBAR_ACCESS_TOKEN environment variable is not set. Not logging anything to Rollbar.');
+            rollbar = null;
+        }
 
         return {
             errorRequest: function (error, request) {
+                if (!rollbar) {
+                    return;
+                }
+
                 var req = {
                     headers: request.headers,
                     protocol: request.uri.protocol,
@@ -22,6 +35,10 @@
                 rollbar.error(error, req);
             },
             error: function (error) {
+                if (!rollbar) {
+                    return;
+                }
+
                 rollbar.error(error);
             }
         }
