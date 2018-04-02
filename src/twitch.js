bae3842cd059b88ddbbbf26e70bac5704db378ec
Peter Han <git@peterhan.me>
2018-04-01 18:52:29 -0700

    Sync the error message and create a fall back

--- src/twitch.js
+++ src/twitch.js
@@ -16,10 +16,10 @@
                     return resp;
                 }
                 else {
-                    var errMsg = [resp.statusCode, resp.statusMessage, ':', resp.body.message].join(' ');
+                    var errMsg = [resp.statusCode, resp.statusMessage, ':', resp.body.message || resp.body].join(' ');
                     var err = new Error(errMsg);
                     rollbar.errorRequest(err, resp.request);
-                    logger.error('Received an error from Twitch:', resp.statusCode, resp.statusMessage, resp.body);
+                    logger.error('Received an error from Twitch:', errMsg);
                     throw err;
                 }
             });
