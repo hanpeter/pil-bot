8decdfab9ddb611fa46d3a74ac8af9175779ba35
Peter Han <git@peterhan.me>
2018-07-06 18:05:33 -0700

    Adding skeleton tests

---
+++ test/twitch.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    /* eslint-disable no-unused-vars, no-empty-function */
+    var mocha = require('mocha');
+    var sinon = require('sinon');
+
+    var module = '../src/twitch.js';
+
+    mocha.describe('Twitch', function () {
+
+    });
+    /* eslint-enable */
+})();
